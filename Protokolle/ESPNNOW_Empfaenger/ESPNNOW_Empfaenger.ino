b062c00b78e691b99dc20530b1b4f67a1ae2bc33
Danit2 <71522810+Danit2@users.noreply.github.com>
2024-12-07 18:50:43 +0100

    Update ESPNNOW_Empfaenger.ino

--- Protokolle/ESPNNOW_Empfaenger/ESPNNOW_Empfaenger.ino
+++ Protokolle/ESPNNOW_Empfaenger/ESPNNOW_Empfaenger.ino
@@ -1,10 +1,10 @@
 /*
-Project:  ESP-NOW Empfänger
-Author:   Thomas Edlinger for www.edistechlab.com
-Date:     Created 06.03.2024
-Version:  V1.0
+Project:  ESP-NOW Empfänger
+Author:   Thomas Edlinger for www.edistechlab.com
+Date:     Created 06.03.2024
+Version:  V1.0
 IDE:      Arduino IDE 2.3.2
- 
+
 Required Board (Tools -> Board -> Boards Manager...)
  - Board: Arduino ESP32 Boards - by Arduino   V2.0.13
 
@@ -15,18 +15,32 @@ Required libraries (sketch -> include library -> manage libraries)
 #include <esp_now.h>
 #include <WiFi.h>
 
-const byte LEDPin = 2; //D2
+// Pins for nine LEDs
+const byte LEDPins[] = {2, 4, 5, 12, 13, 14, 15, 16, 17};  // Array with LED pins
 
-// Define data structure
+// Define data structure with 9 inputs (for 9 LEDs)
 typedef struct struct_message {
   byte a;
+  byte b;
+  byte c;
+  byte d;
+  byte e;
+  byte f;
+  byte g;
+  byte h;
+  byte i;
 } struct_message;
 
 struct_message myData;
 
 void setup() {
   Serial.begin(115200);
-  pinMode (LEDPin, OUTPUT);
+
+  // Set LED pins as OUTPUT
+  for (int i = 0; i < 9; i++) {
+    pinMode(LEDPins[i], OUTPUT);
+  }
+
   WiFi.mode(WIFI_STA);
 
   if (esp_now_init() != ESP_OK) {
@@ -36,14 +50,27 @@ void setup() {
   esp_now_register_recv_cb(OnDataRecv);
 }
 
-// callback function that will be executed when data is received
+// Callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Button: ");
-  Serial.println(myData.a);
+  Serial.println("Received Data:");
+
+  // Display received data in serial monitor for debugging
+  Serial.printf("a: %d, b: %d, c: %d, d: %d, e: %d, f: %d, g: %d, h: %d, i: %d\n", 
+    myData.a, myData.b, myData.c, myData.d, myData.e, myData.f, myData.g, myData.h, myData.i);
+
+  // Control LEDs based on received data
+  digitalWrite(LEDPins[0], myData.a == 1 ? HIGH : LOW);
+  digitalWrite(LEDPins[1], myData.b == 1 ? HIGH : LOW);
+  digitalWrite(LEDPins[2], myData.c == 1 ? HIGH : LOW);
+  digitalWrite(LEDPins[3], myData.d == 1 ? HIGH : LOW);
+  digitalWrite(LEDPins[4], myData.e == 1 ? HIGH : LOW);
+  digitalWrite(LEDPins[5], myData.f == 1 ? HIGH : LOW);
+  digitalWrite(LEDPins[6], myData.g == 1 ? HIGH : LOW);
+  digitalWrite(LEDPins[7], myData.h == 1 ? HIGH : LOW);
+  digitalWrite(LEDPins[8], myData.i == 1 ? HIGH : LOW);
 }
 
 void loop() {
-  if (myData.a == 0) {digitalWrite(LEDPin, LOW);}
-  if (myData.a == 1) {digitalWrite(LEDPin, HIGH);}
+  // No specific loop functionality, as LEDs are controlled by received data
 }
